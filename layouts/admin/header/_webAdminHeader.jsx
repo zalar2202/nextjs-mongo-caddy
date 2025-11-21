c4486b8449d4f5f7ac1c1bf49f953f1ff2be3a8c
Salar Safayi <108089493+zalar2202@users.noreply.github.com>
2025-11-22 01:08:28 +0300

    Update website links in admin header

--- layouts/admin/header/_webAdminHeader.jsx
+++ layouts/admin/header/_webAdminHeader.jsx
@@ -56,12 +56,12 @@ export default function WebAdminHeader(props) {
                             </Link>
                         </li>
                         <li className="menu-item link-item">
-                            <a href="https://omidarmigrate.com/">
-                                وبسایت امیدار
+                            <a href="https://vistauni.com/">
+                                وبسایت ویستا
                             </a>
                         </li>
                         <li className="menu-item link-item">
-                            <a href="https://omidarmigrate.com/blog/">
+                            <a href="https://vistauni.com/blog/">
                                 مجله مهاجرت
                             </a>
                         </li>
